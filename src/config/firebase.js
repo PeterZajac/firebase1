8d5cc2f2c7fd50fe915e025e0c7317127cf82cb2
PeterZajac <zajacp011@gmail.com>
2024-03-04 20:55:56 +0100

    Refactor App component and remove unnecessary files

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeXv3dNXpBu-9XJSIIFGT-wQt46LdHu1c",
+  authDomain: "fir-1-5fc8c.firebaseapp.com",
+  projectId: "fir-1-5fc8c",
+  storageBucket: "fir-1-5fc8c.appspot.com",
+  messagingSenderId: "423621441219",
+  appId: "1:423621441219:web:84c1f6f29765c503b05476",
+  measurementId: "G-2BT446LSRQ",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
